92e03f43dda534a42b8e1fdb10e0e1cbb8cc8aae
Evan <evjava@gmail.com>
2021-07-20 15:28:21 -0400

    npm update

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = {
   pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: ['C:\\web-projects\\vue-login-template-public\\client\\src\\styles\\global.scss']
     }
   }
 };
